beb968cac4235c0a6f3e43dd63e7e5380f9f2069
Zing <zingimmick@outlook.com>
2021-05-23 15:00:20 +0800

    Coding Standard 4 (#28)

--- rector.php
+++ rector.php
@@ -12,34 +12,25 @@ use Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToC
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Zing\CodingStandard\Set\RectorSetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(RectorSetList::CUSTOM);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
+    $containerConfigurator->import(SetList::PHP_70);
+    $containerConfigurator->import(SetList::PHP_71);
+    $containerConfigurator->import(SetList::PHP_72);
+
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(
-        Option::SETS,
-        [
-            SetList::CODING_STYLE,
-            SetList::CODE_QUALITY,
-            SetList::CODE_QUALITY_STRICT,
-            SetList::DEAD_CODE,
-            SetList::PRIVATIZATION,
-            SetList::NAMING,
-            PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-            SetList::PHP_70,
-            SetList::PHP_71,
-            SetList::PHP_72,
-            SetList::EARLY_RETURN,
-        ]
-    );
     $parameters->set(
         Option::SKIP,
         [
-            FinalizeClassesWithoutChildrenRector::class,
-            ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
-            AddSeeTestAnnotationRector::class,
             VarConstantCommentRector::class,
             RemoveUselessParamTagRector::class,
             RemoveUselessReturnTagRector::class,
+            AddSeeTestAnnotationRector::class,
+            ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
+            FinalizeClassesWithoutChildrenRector::class,
         ]
     );
     $parameters->set(
