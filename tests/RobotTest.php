2d0ac8e3d7e7c9f21441a6ccebd223edf25d9a96
zingimmick <zingimmick@users.noreply.github.com>
2021-08-04 12:13:39 +0000

    Rector Rectify

--- tests/RobotTest.php
+++ tests/RobotTest.php
@@ -41,10 +41,10 @@ class RobotTest extends TestCase
                 ))->at('150XXXXXXXX');
             },
             function () {
-                return (new MarkdownMessage(
+                return new MarkdownMessage(
                     '杭州天气',
                     "#### 杭州天气 \n> 9度，西北风1级，空气良89，相对温度73%\n> ![screenshot](https://img.alicdn.com/tfs/TB1NwmBEL9TBuNjy1zbXXXpepXa-2400-1218.png)\n> ###### 10点20分发布 [天气](https://www.dingtalk.com) \n"
-                ));
+                );
             },
             function () {
                 return new LinkMessage(
